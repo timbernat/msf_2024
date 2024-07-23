2a09af7b3da58f1c0b07c0fc19e092800c4f4051
timbernat <tibe3324@colorado.edu>
2024-07-23 15:03:26 -0400

    Separated utility functions from .functions into separate submodules

--- msf_2024/__init__.py
+++ msf_2024/__init__.py
@@ -1,7 +1,11 @@
-"""A smaple package for the MSF 2024 bootcamp"""
+"""A sample package for the MSF 2024 bootcamp"""
 
 # Add imports here
-from .functions import *
+from .functions import canvas
 
+from .measure import calculate_angle, calculate_distance
+from .visualize import bond_histogram, draw_molecule
+from .molecule import build_bond_list
+from . import io
 
 from ._version import __version__
